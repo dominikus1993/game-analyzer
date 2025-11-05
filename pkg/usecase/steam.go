0fce6fb624b6c79ddfdd1f7cc0b5fc7dd5fe59c0
Dominik.Kotecki <dominikus1910@gmail.com>
2025-11-05 17:53:03 +0100

    add simple use case

--- pkg/usecase/steam.go
+++ pkg/usecase/steam.go
@@ -1,1 +1,15 @@
 package usecase
+
+import "context"
+
+type AnalyzeGamesUseCase struct {
+	// Dependencies would be injected here
+}
+
+func NewAnalyzeGamesUseCase() *AnalyzeGamesUseCase {
+	return &AnalyzeGamesUseCase{}
+}
+
+func (uc *AnalyzeGamesUseCase) Execute(context.Context) error {
+	return nil
+}
